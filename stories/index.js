4204b3d930d8f209635841196e43924e628ac317
Gabriel Ulloa <gsulloa@uc.cl>
2018-10-11 22:57:02 -0300

    :sparkle: :wrench: chore(storybook): install and config storybook

---
+++ stories/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+const Button = ({ onClick, children, ...props }) => (
+  <button onClick={onClick} {...props}>
+    {children}
+  </button>
+)
+
+storiesOf("Button0", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))
+
+storiesOf("Button1", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))

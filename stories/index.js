7955e381bc62a097bd4e91e29c8cf5b6069e771c
Juan Cortes <jicortes2@uc.cl>
2018-10-14 22:08:49 -0300

    :sparkles: feat(loginbox): contentbox and signform components

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,9 @@
 import postListBook from "./PostList"
 import postElementBook from "./PostElement"
+import ContentBoxBook from "./ContentBox"
+import SignFormBook from "./SignForm"
 
 postElementBook()
 postListBook()
+ContentBoxBook()
+SignFormBook()

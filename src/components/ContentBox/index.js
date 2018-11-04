5eedcfa47651b1e000d2fbd10af2d270f24de0b7
Gabriel Ulloa <gsulloa@uc.cl>
2018-11-04 19:41:29 -0300

    remove unused import

--- src/components/ContentBox/index.js
+++ src/components/ContentBox/index.js
@@ -4,7 +4,6 @@ import { withStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardActions from "@material-ui/core/CardActions"
 import CardContent from "@material-ui/core/CardContent"
-import Typography from "@material-ui/core/Typography"
 import Divider from "@material-ui/core/Divider"
 
 const styles = {
